ea07a5190a9cd1833d438063fc10deb6bcfbb580
Vinicius Faria <viniciusfaria.vmf@gmail.com>
2023-12-20 23:32:16 -0300

    beta version

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
+import { GlobalStyle } from './styles/global'
+import { DefaultTheme } from './styles/themes/default'
+
+export function App() {
+  return (
+    <ThemeProvider theme={DefaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
+
+// @types/styled-components
